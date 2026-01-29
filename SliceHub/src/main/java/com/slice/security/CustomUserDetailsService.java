009d9b217eb49a75d33fb396bf6c6b8efa6892f6
RAHULRAJx007 <rahulx122003@gmail.com>
2026-01-29 22:45:16 +0530

    added CustomUserDetailsService

---
+++ SliceHub/src/main/java/com/slice/security/CustomUserDetailsService.java
@@ -0,0 +1,39 @@
+package com.slice.security;
+
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.slice.model.User;
+import com.slice.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .disabled(!user.isEnabled()) // blocks unverified users
+                .authorities(
+                        user.getRoles()
+                            .stream()
+                            .map(role -> new SimpleGrantedAuthority(role.getName()))
+                            .collect(Collectors.toList())
+                )
+                .build();
+    }
+}
